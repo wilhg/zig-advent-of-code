af6c0b428307fe18ee97837350f64fb07a77f208
Wei Huang <wei.huang@adevinta.com>
2024-08-21 22:31:37 +0200

    sonnet optmized

--- d4.zig
+++ d4.zig
@@ -8,35 +8,29 @@ const Card = struct {
     numbers_you_have: []const u8,
 
     fn calcPoints(self: *const Card) u16 {
-        var map: [100]bool = [_]bool{false} ** 100;
+        var map = std.bit_set.IntegerBitSet(100).initEmpty();
         for (self.winning_numbers) |wn| {
-            map[wn] = true;
+            map.set(wn);
         }
 
         var points: u16 = 0;
         for (self.numbers_you_have) |nyh| {
-            if (map[nyh]) {
-                if (points == 0) {
-                    points = 1;
-                } else {
-                    points *= 2;
-                }
+            if (map.isSet(nyh)) {
+                points = if (points == 0) 1 else points * 2;
             }
         }
         return points;
     }
 
     fn nextAccCards(self: *const Card) u16 {
-        var map: [100]bool = [_]bool{false} ** 100;
+        var map = std.bit_set.IntegerBitSet(100).initEmpty();
         for (self.winning_numbers) |wn| {
-            map[wn] = true;
+            map.set(wn);
         }
 
         var n: u16 = 0;
         for (self.numbers_you_have) |nyh| {
-            if (map[nyh]) {
-                n += 1;
-            }
+            if (map.isSet(nyh)) n += 1;
         }
         return n;
     }
@@ -48,9 +42,9 @@ const CardSet = struct {
 };
 
 pub fn main() !void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    defer _ = gpa.deinit();
-    const allocator = gpa.allocator();
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
 
     const file = try std.fs.cwd().openFile("d4_input.txt", .{});
     defer file.close();
@@ -60,13 +54,6 @@ pub fn main() !void {
 
     var card_sets = std.ArrayList(CardSet).init(allocator);
     defer card_sets.deinit();
-    defer {
-        // Free the memory allocated for the number slices
-        for (card_sets.items) |card_set| {
-            allocator.free(card_set.card.winning_numbers);
-            allocator.free(card_set.card.numbers_you_have);
-        }
-    }
 
     var buf: [128]u8 = undefined;
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
@@ -74,35 +61,19 @@ pub fn main() !void {
         try card_sets.append(CardSet{ .card = card, .amount = 1 });
     }
 
-    var sum: u16 = 0;
-    for (card_sets.items) |card_set| {
-        sum += card_set.card.calcPoints();
-    }
-    std.debug.print("{}\n", .{sum});
-
+    var sum: u32 = 0;
     var amount_of_cards: u32 = 0;
-    for (0..card_sets.items.len) |i| {
-        var card_set = card_sets.items[i];
+
+    for (card_sets.items, 0..) |*card_set, i| {
+        sum += card_set.card.calcPoints();
         amount_of_cards += card_set.amount;
-        std.debug.print("{}\n", .{card_set.amount});
         for (0..card_set.card.nextAccCards()) |j| {
             card_sets.items[i + j + 1].amount += card_set.amount;
         }
     }
-    std.debug.print("{}\n", .{amount_of_cards});
 
-    // Print the parsed cards for verification
-    // for (cards.items) |card| {
-    //     std.debug.print("Card {}: Winning numbers: ", .{card.id});
-    //     for (card.winningNumbers) |num| {
-    //         std.debug.print("{} ", .{num});
-    //     }
-    //     std.debug.print("| Numbers you have: ", .{});
-    //     for (card.numbersYouHave) |num| {
-    //         std.debug.print("{} ", .{num});
-    //     }
-    //     std.debug.print("\n", .{});
-    // }
+    std.debug.print("Sum of points: {}\n", .{sum});
+    std.debug.print("Total scratchcards: {}\n", .{amount_of_cards});
 }
 
 fn parseCard(allocator: std.mem.Allocator, line: []const u8) !Card {
@@ -130,7 +101,7 @@ fn parseCard(allocator: std.mem.Allocator, line: []const u8) !Card {
 
 fn parseNumbers(allocator: std.mem.Allocator, numbers_str: []const u8) ![]const u8 {
     var numbers = std.ArrayList(u8).init(allocator);
-    defer numbers.deinit();
+    errdefer numbers.deinit();
 
     var num_iter = std.mem.tokenize(u8, numbers_str, " ");
     while (num_iter.next()) |num_str| {
