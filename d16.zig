40cbf66e9a42800088322346db1233f44d5da933
wilhg <wilhg@pm.me>
2024-09-01 00:24:42 +0200

    d16p1 half

--- d16.zig
+++ d16.zig
@@ -1,3 +1,104 @@
-// 一束光的起点：左上角或者分光器；终点：分光器，墙壁，开始重合于光束
-// 子光束继承父光束既有路径
-// 父光束和兄弟光束不知道子光束的存在
+const std = @import("std");
+
+const SIZE = 110;
+
+const Tile = struct {
+    position: Position,
+    mirror: u8,
+    charged: bool,
+};
+
+const Grid = struct {
+    tiles: [SIZE][SIZE]Tile,
+};
+
+const Direction = enum { Up, Down, Left, Right };
+
+const Position = struct { y: usize, x: usize };
+
+const PassResult = struct {
+    one: *Beam,
+    two: ?*Beam,
+};
+
+fn one(beam: *Beam) PassResult {
+    return PassResult{ .one = beam, .two = null };
+}
+
+fn two(a: *Beam, b: *Beam) PassResult {
+    return PassResult{ .one = a, .two = b };
+}
+
+const Beam = struct {
+    start: Position,
+    direction: Direction,
+
+    fn birth(self: *Beam, dir: Direction) *Beam {
+        return &Beam{
+            .start = self.start,
+            .direction = dir,
+        };
+    }
+
+    fn pass(self: *Beam, tile: *Tile) PassResult {
+        tile.charged = true;
+
+        return switch (tile.mirror) {
+            '.' => one(self),
+
+            '/' => one(self.birth(switch (self.direction) {
+                .Up => .Right,
+                .Down => .Left,
+                .Left => .Up,
+                .Right => .Down,
+            })),
+
+            '\\' => one(self.birth(switch (self.direction) {
+                .Up => .Left,
+                .Down => .Right,
+                .Left => .Down,
+                .Right => .Up,
+            })),
+
+            '-' => {
+                if (self.direction == .Left or self.direction == .Right) {
+                    return one(self);
+                } else {
+                    return two(self.birth(.Up), self.birth(.Down));
+                }
+            },
+
+            '|' => {
+                if (self.direction == .Up or self.direction == .Down) {
+                    return one(self);
+                } else {
+                    return two(self.birth(.Left), self.birth(.Right));
+                }
+            },
+
+            else => unreachable,
+        };
+    }
+};
+
+pub fn main() !void {
+    const input = @embedFile("d16_input.txt");
+    var grid: Grid = undefined;
+
+    var y: usize = 0;
+    var lines = std.mem.split(u8, input, "\n");
+    while (lines.next()) |line| : (y += 1) {
+        for (line, 0..) |char, x| {
+            grid.tiles[y][x] = Tile{ .mirror = char, .charged = false };
+        }
+    }
+
+    // Print the parsed input
+    for (0..SIZE) |row| {
+        for (0..SIZE) |col| {
+            std.debug.print("{c}", .{grid.tiles[row][col].mirror});
+        }
+        std.debug.print("\n", .{});
+    }
+    std.debug.print("\n", .{});
+}
