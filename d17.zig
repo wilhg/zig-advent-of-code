bd31c563b7957e82bf3e54627ba0b1f51aabf283
wilhg <wilhg@pm.me>
2024-09-02 00:29:45 +0200

    failed

--- d17.zig
+++ d17.zig
@@ -4,6 +4,7 @@ const SIZE: usize = 141;
 
 var weights: [SIZE][SIZE]u16 = undefined;
 var scores: [SIZE][SIZE]u16 = undefined;
+var done: [SIZE][SIZE]bool = undefined;
 
 const Position = struct {
     y: usize,
@@ -23,49 +24,81 @@ fn initData() void {
 
     for (0..SIZE) |y| {
         for (0..SIZE) |x| {
-            scores[y][x] = 0xFF; // Set to maximum value for u16
+            scores[y][x] = 0xFF;
+            done[y][x] = false;
         }
     }
+
+    scores[0][0] = weights[0][0];
 }
 
-fn go(p: Position, last_dir: Direction, straight_count: u2) void {
+fn bfs(p: Position, last_dir: Direction, straight_count: u2) void {
+    if (done[p.y][p.x]) return;
+
+    std.debug.print("p: {}, {}, last_dir: {}, straight_count: {d}\n", .{ p.y, p.x, last_dir, straight_count });
+
     // up
+    var sc_up: ?u2 = null;
+    std.debug.print("up\n", .{});
     if (p.y > 0 and last_dir != .down and (last_dir != .up or (last_dir == .up and straight_count < 3))) {
         const np = Position{ .y = p.y - 1, .x = p.x };
-        scores[np.y][np.x] = @min(scores[np.y][np.x], scores[p.y][p.x] + weights[np.y][np.x]);
-        go(np, .up, if (last_dir != .up) 0 else straight_count + 1);
+        if (!done[np.y][np.x]) {
+            scores[np.y][np.x] = @min(scores[np.y][np.x], scores[p.y][p.x] + weights[np.y][np.x]);
+            sc_up = if (last_dir != .up) 0 else straight_count + 1;
+        }
     }
 
     // down
+    var sc_down: ?u2 = null;
+    std.debug.print("down\n", .{});
     if (p.y < SIZE - 1 and last_dir != .up and (last_dir != .down or (last_dir == .down and straight_count < 3))) {
         const np = Position{ .y = p.y + 1, .x = p.x };
-        scores[np.y][np.x] = @min(scores[np.y][np.x], scores[p.y][p.x] + weights[np.y][np.x]);
-        go(np, .down, if (last_dir != .down) 0 else straight_count + 1);
+        if (!done[np.y][np.x]) {
+            scores[np.y][np.x] = @min(scores[np.y][np.x], scores[p.y][p.x] + weights[np.y][np.x]);
+            sc_down = if (last_dir != .down) 0 else straight_count + 1;
+        }
     }
 
     // left
+    var sc_left: ?u2 = null;
+    std.debug.print("left\n", .{});
     if (p.x > 0 and last_dir != .right and (last_dir != .left or (last_dir == .left and straight_count < 3))) {
         const np = Position{ .y = p.y, .x = p.x - 1 };
-        scores[np.y][np.x] = @min(scores[np.y][np.x], scores[p.y][p.x] + weights[np.y][np.x]);
-        go(np, .left, if (last_dir != .left) 0 else straight_count + 1);
+        if (!done[np.y][np.x]) {
+            scores[np.y][np.x] = @min(scores[np.y][np.x], scores[p.y][p.x] + weights[np.y][np.x]);
+            sc_left = if (last_dir != .left) 0 else straight_count + 1;
+        }
     }
 
     // right
+    var sc_right: ?u2 = null;
+    std.debug.print("right\n", .{});
     if (p.x < SIZE - 1 and last_dir != .left and (last_dir != .right or (last_dir == .right and straight_count < 3))) {
         const np = Position{ .y = p.y, .x = p.x + 1 };
-        scores[np.y][np.x] = @min(scores[np.y][np.x], scores[p.y][p.x] + weights[np.y][np.x]);
-        go(np, .right, if (last_dir != .right) 0 else straight_count + 1);
+        if (!done[np.y][np.x]) {
+            scores[np.y][np.x] = @min(scores[np.y][np.x], scores[p.y][p.x] + weights[np.y][np.x]);
+            sc_right = if (last_dir != .right) 0 else straight_count + 1;
+        }
     }
+
+    std.debug.print("done\n", .{});
+
+    done[p.y][p.x] = true;
+
+    if (sc_up) |n| bfs(Position{ .y = p.y - 1, .x = p.x }, .up, n);
+    if (sc_down) |n| bfs(Position{ .y = p.y + 1, .x = p.x }, .down, n);
+    if (sc_left) |n| bfs(Position{ .y = p.y, .x = p.x - 1 }, .left, n);
+    if (sc_right) |n| bfs(Position{ .y = p.y, .x = p.x + 1 }, .right, n);
 }
 
 pub fn main() !void {
     initData();
 
-    go(Position{ .y = 0, .x = 0 }, .down, 0);
+    bfs(Position{ .y = 0, .x = 0 }, .down, 1);
 
     for (scores) |row| {
         for (row) |tile| {
-            std.debug.print("{d}\t", .{tile});
+            std.debug.print("{x}", .{tile & 0xF});
         }
         std.debug.print("\n", .{});
     }
